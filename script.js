e99e6d9cf0632e8984f11526bd9ae19d3b742eee
thaitran <43898236+thaitranxyz@users.noreply.github.com>
2023-02-15 18:50:24 +1100

    Init

---
+++ script.js
@@ -0,0 +1,23 @@
+const musicContainer = document.querySelector('.music-container');
+const playBtn = document.querySelector('#play');
+const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
+const audio = document.querySelector('#audio');
+const progress = document.querySelector('.progress');
+const progressContainer = document.querySelector('.progress-container');
+const title = document.querySelector('#title');
+const cover = document.querySelector('#cover');
+
+const songs = ['hey', 'summer', 'ukulele'];
+
+let songIndex = 1;
+
+loadSong(songs[songIndex]);
+
+function loadSong(song) {
+    title.innerText = song;
+    audio.src = `music/${song}.mp3`;
+    cover.src = `images/${song}.jpg`;
+}
+
+ 
